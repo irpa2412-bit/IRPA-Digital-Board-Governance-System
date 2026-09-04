87e39610aa41d4eb767f3d334597b5fe6262aa51
irpa2412-bit <irpa2412@gmail.com>
2026-09-04 22:23:33 +0300

    Refactor Firebase config and update credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,29 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import {
+  getAuth,
+  GoogleAuthProvider
+} from "firebase/auth";
+import {
+  getFirestore
+} from "firebase/firestore";
+import {
+  getStorage
+} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaS5B5O2s3T8fTVsgD4s3I_4PbnQ_Mua0-C_E",
-  authDomain: "irpa-digital-governance-system.firebaseapp.com",
-  projectId: "irpa-digital-governance-system",
-  storageBucket: "irpa-digital-governance-system.firebasestorage.app",
-  messagingSenderId: "467032575367",
-  appId: "1:467032575367:web:3b6d65b79d2a490bde5722",
-  measurementId: "G-LBVG6ES5CJ"
+  apiKey: "AIzaS2C2aMdxHD14nMnGiRyf4mSL1ixXdzBoOtE",
+  authDomain:
+    "irpa-digital-board-governance.firebaseapp.com",
+  projectId:
+    "irpa-digital-board-governance",
+  storageBucket:
+    "irpa-digital-board-governance.firebasestorage.app",
+  messagingSenderId:
+    "217055978789",
+  appId:
+    "1:217055978789:web:937d1f2f781202cc1e26cc",
+  measurementId:
+    "G-XDJEBRBEVC"
 };
 
 export const GOOGLE_OAUTH_CLIENT_ID =
@@ -19,9 +32,13 @@ export const GOOGLE_OAUTH_CLIENT_ID =
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+
 export const storage = getStorage(app);
-export const googleProvider = new GoogleAuthProvider();
+
+export const googleProvider =
+  new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
   prompt: "select_account"
