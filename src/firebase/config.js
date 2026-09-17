5b76ab7b8b311d6f4394e1bd00330e8fd214a097
irpa2412-bit <irpa2412@gmail.com>
2026-09-17 22:21:24 +0300

    Remove Firebase Storage client dependency for Google Drive document storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,6 @@
-import {
-  initializeApp
-} from "firebase/app";
-
-import {
-  getAuth,
-  GoogleAuthProvider
-} from "firebase/auth";
-
-import {
-  getFirestore
-} from "firebase/firestore";
-
-import {
-  getStorage
-} from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyC2aMdxHD14nMnGiRyf4mSL1ixXdzBoOtE",
@@ -31,11 +18,7 @@ export const GOOGLE_OAUTH_CLIENT_ID =
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
-
-export const storage = getStorage(app);
-
 export const googleProvider = new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
