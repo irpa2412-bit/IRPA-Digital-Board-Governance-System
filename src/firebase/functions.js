e75217bd075ce00359d71b36553d7eac0ce920a9
irpa2412-bit <irpa2412@gmail.com>
2026-09-22 00:50:09 +0300

    Fix administrator callable export formatting

--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -2,4 +2,5 @@ import { getFunctions, httpsCallable } from "firebase/functions";
 import app from "./config";
 
 export const functions = getFunctions(app, "us-central1");
-\nexport async function createAdministrator(data){ const call=httpsCallable(functions,"createAdministrator"); const result=await call(data); return result.data; }\n
+
+export async function createAdministrator(data){ const call=httpsCallable(functions,"createAdministrator"); const result=await call(data); return result.data; }
