b1a2d26a1e5c73c01aa4a80dcd7ce1fa3a62f563
irpa2412-bit <irpa2412@gmail.com>
2026-09-22 00:50:04 +0300

    Expose secure administrator provisioning to admin gateway

--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -1,4 +1,5 @@
-import { getFunctions } from "firebase/functions";
+import { getFunctions, httpsCallable } from "firebase/functions";
 import app from "./config";
 
 export const functions = getFunctions(app, "us-central1");
+\nexport async function createAdministrator(data){ const call=httpsCallable(functions,"createAdministrator"); const result=await call(data); return result.data; }\n
